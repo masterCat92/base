8d1d2689622791d59091d5033275b91da38d1634
masterCat92 <62793395+masterCat92@users.noreply.github.com>
2020-04-15 22:31:39 +0800

    [ADD] 雪花算法，树（未完成）
    雪花算法，树（未完成）

---
+++ src/main/java/org/sunny/base/algorithm/tree/BinaryTree.java
@@ -0,0 +1,48 @@
+package org.sunny.base.algorithm.tree;
+
+/**
+ * @ClassName BinaryTree
+ * @Description: TODO
+ * @Author sunzhen
+ * @Date 2020/4/15
+ * @Version V1.0
+ **/
+public class BinaryTree<T> {
+    public Node<T> root;//根
+
+    //清除树
+    public void clear() {
+        clear(root);
+    }
+
+    //清除某一节点
+    public void clear(Node<T> node) {
+        if (null != node) {
+            clear(node.getLeftChild());
+            clear(node.getRightChild());
+            node = null;
+        }
+    }
+
+    //树是否为空
+    public boolean isEmpty() {
+        return null == root;
+    }
+
+    //---------------------遍历---------
+
+    /**
+     * 前序遍历(递归)
+     * 访问根节点->根遍历左子树->根遍历右子树->退出
+     * 根左右，深度遍历
+     */
+    public void preOrder(Node<T> node) {
+        if (null != node) {
+            System.out.print(node.getData() + "");
+            preOrder(node.getLeftChild());
+            preOrder(node.getRightChild());
+        }
+    }
+
+
+}
---
+++ src/main/java/org/sunny/base/algorithm/tree/Node.java
@@ -0,0 +1,46 @@
+package org.sunny.base.algorithm.tree;
+
+/**
+ * @ClassName Node
+ * @Description: 二叉树节点
+ * @Author sunzhen
+ * @Date 2020/4/15
+ * @Version V1.0
+ **/
+public class Node<T> {
+
+    private T data;//节点数据
+    private Node<T> leftChild;//左子节点
+    private Node<T> rightChild;//右子节点
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public Node() {
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(Node<T> leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public Node<T> getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(Node<T> rightChild) {
+        this.rightChild = rightChild;
+    }
+}
